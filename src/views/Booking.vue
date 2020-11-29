c7874f96242a15ce2b1f919b4552a20c1d801dde
vuvi <gusvuvij@student.gu.se>
2020-11-29 11:17:28 +0100

    Fix appointment booking form
    We added the required input fields into the appointment form. We also changed the input fields to all strings for now.

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,24 +1,57 @@
 <template>
-    <div>
-        <b-container>
-        <h1>Appointment Booking Form</h1>
-        <form>
+  <div>
+    <b-container>
+      <h1>Appointment Booking Form</h1>
+      <form>
         <label>User ID </label>
-        <b-form-input v-model="message.name" placeholder="Enter your name"></b-form-input>
+        <b-form-input
+          v-model="message.name"
+          placeholder="Enter your user ID"
+        ></b-form-input>
 
-        <label>Dentist </label>
-        <b-form-select v-model="message.dentist" :options="dentistId"></b-form-select>
+         <label>Request ID </label>
+        <b-form-input
+          v-model="message.requestId"
+          placeholder="Enter your request ID"
+        ></b-form-input>
 
-       <label>Date </label>
-        <b-form-datepicker id="date-blah" v-model="message.date"></b-form-datepicker>
+        <label>Dentist ID </label>
+        <b-form-select
+          v-model="message.dentistId"
+          :options="dentistId"
+          placeholder="Enter your dentist ID"
+
+        ></b-form-select>
+
+         <label>Issuance Number </label>
+        <b-form-input
+          v-model="message.issuance"
+          placeholder="Enter your issuance number"
+        ></b-form-input>
+
+        <label>Date and Time</label>
+        <b-form-input
+          v-model="message.time"
+          placeholder="Enter your appointment date and time"
+        ></b-form-input>
+
+        <!-- For calender and appointment time
+        <label>Date and Time </label>
+        <b-form-datepicker
+          id="date-blah"
+          v-model="message.date"
+        ></b-form-datepicker>
 
         <label>Appointment Time </label>
-        <b-form-select v-model="message.priority" :options="appointmentTimes"></b-form-select>
+        <b-form-select
+          v-model="message.priority"
+          :options="appointmentTimes"
+        ></b-form-select> -->
 
-        <b-button >Submit Booking</b-button>
-        </form>
-        </b-container>
-    </div>
+        <b-button>Submit Booking</b-button>
+      </form>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -29,10 +62,10 @@ export default {
     return {
       message: {
         userId: '',
-        requestId: null,
-        issuance: null,
-        date: '',
-        dentistId: ''
+        requestId: '',
+        dentistId: '',
+        issuance: '',
+        time: ''
       }
     }
   },
