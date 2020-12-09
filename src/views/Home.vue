de4bfde575ffbc3075a19e9ee16ea1dad4adeae4
fayona <fayona@student.chalmers.se>
2020-12-09 16:02:21 +0100

    Add dentist markers to map
    Negin and I tried to add the dentist locations to the map as markers. This doesn't currently work but is a work in progress.
    
    Resolves: #9

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,7 @@
 <script>
 import Paho from '../../libraries/paho.javascript-1.1.0/paho-mqtt.js'
 import mapboxgl from 'mapbox-gl'
+import d3 from 'd3'
 mapboxgl.accessToken = '' // Insert access token here
 
 export default {
@@ -25,7 +26,7 @@ export default {
       center: [11.974560, 57.708870],
       zoom: 12
     })
-    map.location()
+    // var marker = new mapboxgl.Marker()
 
     // Create a client instance
     var client = new Paho.Client(location.hostname, Number(9001), '', 'frontend')
@@ -58,6 +59,62 @@ export default {
     function onMessageArrived(message) {
       console.log('onMessageArrived:' + message.payloadString)
     }
+
+    var mapData = Paho.Message
+
+    map.on('load', function () {
+      // We use D3 to fetch the JSON here so that we can parse and use it separately
+      // from GL JS's use in the added source. You can use any request method (library
+      // or otherwise) that you want.
+      d3.json(mapData,
+        function (err, data) {
+          if (err) throw err
+          // save full coordinate list for later
+          var coordinates = data.dentists.coordinate
+          // start by showing just the first coordinate
+          data.dentists.coordinate = [coordinates[0]]
+          // add it to the map
+          map.addSource('trace', { type: 'geojson', data: data })
+          map.addLayer({
+            id: 'trace',
+            type: 'line',
+            source: 'trace'
+          })
+          // setup the viewport
+          map.jumpTo({ center: coordinates[0], zoom: 14 })
+          map.setPitch(30)
+          // on a regular basis, add more coordinates from the saved list and update the map
+          var i = 0
+          var timer = window.setInterval(function () {
+            if (i < coordinates.length) {
+              data.dentists.coordinate.push(
+                coordinates[i]
+              )
+              map.getSource('trace').setData(data)
+              map.panTo(coordinates[i])
+              i++
+            } else {
+              window.clearInterval(timer)
+            }
+          }, 10)
+        }
+      )
+      // mapData.dentists.forEach(function (dentist, i) {
+      //  dentist.properties.id = i
+      // })
+
+    /* map.on('load', function (e) {
+      map.addLayer({
+        source: {
+          type: 'json',
+          data: mapData
+        },
+        layout: {
+          icon: marker
+        }
+      })
+      marker.addTo(map) */
+    })
   },
   methods: {
     publish() {
