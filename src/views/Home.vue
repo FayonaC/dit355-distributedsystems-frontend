cb038fd56eb2b3b0dd551b2c0da1c1df2cf6777c
nordje <nordje@student.chalmers.se>
2020-12-22 17:40:28 +0100

    Map shows appointment availability
    Negin, Fay and I added functionality for displaying the markers depending on the availability of time slots. The button will become disabled when there are no available slots.
    
    Resolves #17

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,11 +30,18 @@ export default {
       availabilityRequest: {
         date: null
       },
-      message: 'none'
+      message: 'none',
+      dentists: [],
+      schedules: [],
+      map: {},
+      unavailable: [],
+      layerGroup: {}
     }
   },
   mounted() {
-    var map = L.map('map').setView([57.708870, 11.974560], 12)
+    this.subscribe()
+
+    this.map = L.map('map').setView([57.708870, 11.974560], 12)
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -43,71 +50,46 @@ export default {
       tileSize: 512,
       zoomOffset: -1,
       accessToken: 'pk.eyJ1IjoiZGlzdHJpYnN5cyIsImEiOiJja2llZml2aG0xc2dxMnhvNW55bm1hd3U1In0.V731WqpeBOC6a8wwZUwwAA' // Insert access token here
-    }).addTo(map)
+    }).addTo(this.map)
 
-    // set callback handlers
-    client.onConnectionLost = onConnectionLost
-    client.onMessageArrived = onMessageArrived
-
-    // connect the client
-    client.connect({ onSuccess: onConnect })
-
-    // called when the client connects
-    function onConnect() {
-      // Once a connection has been made, make a subscription to a topic and send a message.
-      console.log('Connected')
-      var subOptions = {
-        qos: 1
-      }
-      client.subscribe('Dentists', subOptions)
-      var message = new Paho.Message('Hello from dentists')
-      message.destinationName = 'Dentist'
-      message.qos = 1
-      client.send(message)
-
-    /* client.subscribe('free-slots') // This might need to change
-      var messageTwo = new Paho.Message('Hello from availability')
-      messageTwo.destinationName = 'Availability'
-      client.send(messageTwo) */
-    }
-
-    // called when the client loses its connection
-    function onConnectionLost(responseObject) {
-      if (responseObject.errorCode !== 0) {
-        console.log('onConnectionLost:' + responseObject.errorMessage)
-      }
-    }
-
-    // called when a message arrives
-    function onMessageArrived(message) {
-      console.log('onMessageArrived:' + message.payloadString)
-      var parsing = JSON.parse(message.payloadString) // Parsing the incoming JSON (message)
-      parsing.dentists.forEach(function (dentists, i) { // Iterates through all the dentists in the JSON
-        dentists.id = i
-        var longitudeMap = (parsing.dentists[i].coordinate.longitude) // Saves the longitude in a variable
-        var latitudeMap = (parsing.dentists[i].coordinate.latitude) // Saves the latitude in a variable
-        var marker = L.marker([latitudeMap, longitudeMap]).addTo(map) // Uses the stored coordinates and adds them to the map as markers
-        // getting data we need for the popups
-        var officeName = (parsing.dentists[i].name)
-        var dentistNum = (parsing.dentists[i].dentists)
-        var address = (parsing.dentists[i].address)
-        var city = (parsing.dentists[i].city)
-        var monHours = (parsing.dentists[i].openinghours.monday)
-        var tuesHours = (parsing.dentists[i].openinghours.tuesday)
-        var wedHours = (parsing.dentists[i].openinghours.wednesday)
-        var thursHours = (parsing.dentists[i].openinghours.thursday)
-        var friHours = (parsing.dentists[i].openinghours.friday)
-
-        // adds dental office information as popups on markers
-
-        marker.bindPopup('<b>Dental office:</b> ' + officeName + '<br><b>Address:</b> ' + address + ', <br>' + city +
-        '<br><b>Number of dentists:</b> ' + dentistNum + '<br><b>Opening hours:</b> ' + '<br>Monday: ' + monHours +
-        '<br>Tuesday: ' + tuesHours + '<br>Wednesday: ' + wedHours + '<br>Thursday: ' + thursHours + '<br>Friday: ' +
-        friHours + '<br><a class="btn btn-primary" href="/booking">Book appointment</a>')
-      })
-    }
+    this.layerGroup = L.layerGroup().addTo(this.map)
   },
   methods: {
+    subscribe() {
+      // set callback handlers
+      client.onConnectionLost = onConnectionLost
+      client.onMessageArrived = this.onMessageArrived
+
+      // connect the client
+      client.connect({ onSuccess: onConnect })
+
+      // called when the client connects
+      function onConnect() {
+        // Once a connection has been made, make a subscription and send a message.
+        console.log('Connected')
+
+        var subOptions = {
+          qos: 1
+        }
+        client.subscribe('Dentists', subOptions)
+        var message = new Paho.Message('Hello from dentists')
+        message.destinationName = 'Dentist'
+        message.qos = 1
+        client.send(message)
+
+        client.subscribe('free-slots')
+        var messageTwo = new Paho.Message('Hello from availability')
+        messageTwo.destinationName = 'Availability'
+        client.send(messageTwo)
+      }
+
+      // called when the client loses its connection
+      function onConnectionLost(responseObject) {
+        if (responseObject.errorCode !== 0) {
+          console.log('onConnectionLost:' + responseObject.errorMessage)
+        }
+      }
+    },
     publish() {
       // Creates an availability request
       var availabilityRequest = {
@@ -118,6 +100,89 @@ export default {
       message.topic = 'AvailabilityRequest'
       message.qos = 1
       client.publish(message)
+    },
+    onMessageArrived(message) {
+      if (JSON.parse(message.payloadString).dentists) {
+        this.getDentists(message, this.map)
+      } else if (JSON.parse(message.payloadString).schedules) {
+        this.availability(message, this.map)
+      }
+    },
+    availability(message, map) {
+      this.unavailable = []
+      // match dentist id to marker
+      var schedules = JSON.parse(message.payloadString).schedules
+      this.schedules = schedules
+
+      var createMarkers = this.createMarkers
+      var unavailable = this.unavailable
+
+      schedules.forEach(function (schedule, i) {
+        if (schedule.timeSlots.length === 0) {
+          // Sets dentist id together with availability status
+          var un = {
+            dentist: schedule.dentist
+          }
+          unavailable.push(un)
+        } else {
+          // Sets dentist id together with availability status
+        }
+        createMarkers(map)
+      })
+    },
+    createMarkers(map) {
+      var unavailable = this.unavailable
+      this.layerGroup.clearLayers()
+      var layerGroup = this.layerGroup
+      this.dentists.forEach(function (dentists, i) { // Iterates through all the dentists in the JSON
+        var available = false
+        var longitudeMap = (dentists.coordinate.longitude) // Saves the longitude in a variable
+        var latitudeMap = (dentists.coordinate.latitude) // Saves the latitude in a variable
+        var marker = {}
+        // Match dentist to dentist id in availability and set opacity based on that
+        if (unavailable.length > 0) {
+          unavailable.forEach(function (un, i) {
+            if (un.dentist === dentists.id) {
+              marker = L.marker([latitudeMap, longitudeMap]).setOpacity(0.5).addTo(layerGroup)
+            } else {
+              marker = L.marker([latitudeMap, longitudeMap]).setOpacity(1).addTo(layerGroup) // Uses the stored coordinates and adds them to the map as markers
+              available = true
+            }
+          })
+        } else {
+          marker = L.marker([latitudeMap, longitudeMap]).setOpacity(1).addTo(layerGroup) // Uses the stored coordinates and adds them to the map as markers
+          available = true
+        }
+        // getting data we need for the popups
+        var officeName = (dentists.name)
+        var dentistNum = (dentists.dentists)
+        var address = (dentists.address)
+        var city = (dentists.city)
+        var monHours = (dentists.openinghours.monday)
+        var tuesHours = (dentists.openinghours.tuesday)
+        var wedHours = (dentists.openinghours.wednesday)
+        var thursHours = (dentists.openinghours.thursday)
+        var friHours = (dentists.openinghours.friday)
+
+        // adds dental office information as popups on markers
+
+        marker.bindPopup('<b>Dental office:</b> ' + officeName + '<br><b>Address:</b> ' + address + ', <br>' + city +
+        '<br><b>Number of dentists:</b> ' + dentistNum + '<br><b>Opening hours:</b> ' + '<br>Monday: ' + monHours +
+        '<br>Tuesday: ' + tuesHours + '<br>Wednesday: ' + wedHours + '<br>Thursday: ' + thursHours + '<br>Friday: ' +
+        friHours + getButton(available))
+      })
+
+      function getButton(available) {
+        if (available) {
+          return '<br><a class="btn btn-primary" href="/booking">Book appointment</a>'
+        } else {
+          return '<br><a class="btn btn-primary disabled" href="/booking">Book appointment</a>'
+        }
+      }
+    },
+    getDentists(message, map) {
+      this.dentists = JSON.parse(message.payloadString).dentists // Parsing the incoming JSON (message)
+      this.createMarkers(map, 1)
     }
   }
 }
