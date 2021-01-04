abb2a2ab776fecd4334ca2cb84386410d4f8c4eb
Shonaigh Douglas <shonaigh@student.chalmers.se>
2021-01-04 15:09:31 +0100

    Edit styling of home page
    Negin & Shonaigh removed the About Us page, added a logo to the nav bar, increased the size of the site name. We also made changes to space out the selection boxes and make their appearance more uniform and in line with each other. We increased the size of the confirm button and moved the alert messages to beneath the confirm bookiing button. We changed some of the labels and button text to more accurately reflect what they represented.
    
    Resolves: #25

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     <b-navbar>
       <b-container class="d-flex justify-content-start">
           <div class="col-4">
-            <b-navbar-brand to="/">LOGO</b-navbar-brand>
+            <b-navbar-brand to="/"><img src = "./assets/tooth-pin-white.png" alt="map pin with white tooth" width="50" height="50"></b-navbar-brand>
           </div>
           <div class="col-4 text-center d-flex justify-content-center">
             <b-navbar-nav>
-              <b-nav-item to="/">DentiPicks</b-nav-item>
+              <b-nav-item class="h1" to="/">DentiPicks</b-nav-item>
             </b-navbar-nav>
           </div>
       </b-container>
@@ -18,7 +18,7 @@
         <b-row align-h="end">
           <b-col cols="3 text-right">
             <p>
-              <a target="_blank" href="https://git.chalmers.se/courses/dit355/2020/group-3">GitLab Repository</a> | <router-link to="/aboutus">About Us</router-link>
+              <a target="_blank" href="https://git.chalmers.se/courses/dit355/2020/group-3">GitLab Repository</a>
             </p>
           </b-col>
         </b-row>
